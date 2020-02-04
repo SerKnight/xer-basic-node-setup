59278fdb7a72d20edb6c9026243cb6316faf306e
serknight <chrisknight.mail@gmail.com>
2020-02-04 16:24:55 -0700

    state passing example

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const session = require('express-session');
-const  xero_node = require('xero-node')
+const xero_node = require('xero-node')
 
-const client_id = 'your-client-id'
-const client_secret = 'your-client-secret'
-const redirectUri = 'your-callback-from-developer-portal-oauth2-app-settings'
-const scopes = 'openid profile email accounting.transactions accounting.settings offline_access'
+const client_id = '< replace >'
+const client_secret = '< replace >'
+const redirectUri = 'http://localhost:8080/callback'
+const scopes = 'openid email profile offline_access accounting.transactions accounting.contacts'
 
 const xero = new xero_node.XeroClient({
   clientId: client_id,
@@ -34,7 +34,7 @@ app.get('/', function(req, res) {
 app.get('/connect', async function(req, res) {
   try {
     let consentUrl = await xero.buildConsentUrl();
-    res.redirect(consentUrl + "&state=THIS_IS_A_STANDARD_OAUTH_2_STATE_PARAMETER");
+    res.redirect(consentUrl + "&state=THIS_IS_A_STANDARD_OAUTH_2_STATE_PARAMETER"); // Append any type of state/params you would like
   } catch (err) {
     res.send("Sorry, something went wrong");
   }
@@ -43,20 +43,19 @@ app.get('/connect', async function(req, res) {
 app.get('/callback', async function(req, res) {
   let url = redirectUri + req.originalUrl;
 
-  // We have to call this function until this issue is resolved: https://github.com/XeroAPI/xero-node/issues/317
-  await xero.buildConsentUrl();
+  console.log('req.query: ', req.query) // this will the the same state/params you passed to the API
 
-  // Uncomment following line in order to work-around the current issue: https://github.com/XeroAPI/xero-node/issues/304
-  // url = url.replace(/&state=[^&]*/)
+  await xero.buildConsentUrl();
   await xero.setAccessTokenFromRedirectUri(url);
 
-  // Optional: read user info from the id token
+  // (optional) get user info from the id token
   let tokenClaims = await xero.readIdTokenClaims();
   const accessToken = await xero.readTokenSet();
 
   req.session.tokenClaims = tokenClaims;
   req.session.accessToken = accessToken;
   req.session.xeroTenantId = xero.tenantIds[0];
+
   res.redirect('/organisation');
 })
 
@@ -65,11 +64,11 @@ app.get('/organisation', async function(req, res) {
     const response = await xero.accountingApi.getOrganisations(xero.tenantIds[0])
     res.send("Hello, " + response.body.organisations[0].name);
   } catch (err) {
-    res.send("Sorry, something went wrong");
+    res.send(err);
   }
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, function() {
   console.log("Your Xero basic public app is running at localhost:" + PORT)
 })
